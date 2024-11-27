cbb6600d999cdaa98453661e7fbfcb7761cc880f
Denys Dobrelya <olddba@amazon.co.uk>
2024-11-26 20:55:10 -0800

    Commented out the names
    There are no sensitive values here, but I just commented out the known parameters to accommodate Qualys.

--- source/excludefromgit.py
+++ source/excludefromgit.py
@@ -1,8 +1,8 @@
-# Secrets do be excluded from Git and others.
+# Secrets to be excluded from Git and others.
 
 # Sign cookies for sessions
-nogit_secret_key = 'PlaceYourOwnHere!' 
+#nogit_secret_key = 'PlaceYourOwnHere!' 
 
 # Set these values from the Linkedin app you created
-NOGIT_CONSUMER_KEY = 'PlaceYourOwnHere!'
-NOGIT_CONSUMER_SECRET = 'PlaceYourOwnHere!'
+#NOGIT_CONSUMER_KEY = 'PlaceYourOwnHere!'
+#NOGIT_CONSUMER_SECRET = 'PlaceYourOwnHere!'
